704e5093a911d216b6629648ac89cea9efdc15a1
Susie Kim <susie.kim@hotmail.com>
2019-12-02 17:06:12 -0500

    day2 part2

--- day2/index.js
+++ day2/index.js
@@ -1,5 +1,6 @@
 /* 
 --- Day 2: 1202 Program Alarm ---
+--- Part 1 ---
 On the way to your gravity assist around the Moon, your ship computer beeps angrily about a "1202 program alarm". On the radio, an Elf is already explaining how to handle the situation: "Don't worry, that's perfectly norma--" The ship computer bursts into flames.
 
 You notify the Elves that the computer's magic smoke seems to have escaped. "That computer ran Intcode programs like the gravity assist program it was working on; surely there are enough spare parts up there to build a new Intcode computer!"
@@ -45,8 +46,6 @@ Once you have a working computer, the first step is to restore the gravity assis
 
 const getInputs = require("../helpers/getInputs");
 const inputs = getInputs("./input.txt", ",");
-inputs[1] = 12;
-inputs[2] = 2;
 
 const runProgram = input => {
   for (let i = 0; i < input.length; i += 4) {
@@ -69,5 +68,45 @@ const runProgram = input => {
   return input;
 };
 
-console.log(runProgram(inputs)[0]);
+// inputs[1] = 12;
+// inputs[2] = 2;
+// console.log(runProgram(inputs)[0]); // 5110675
+
+/* --- Part Two ---
+"Good, the new computer seems to be working correctly! Keep it nearby during this mission - you'll probably use it again. Real Intcode computers support many more features than your new one, but we'll let you know what they are as you need them."
+
+"However, your current priority should be to complete your gravity assist around the Moon. For this mission to succeed, we should settle on some terminology for the parts you've already built."
+
+Intcode programs are given as a list of integers; these values are used as the initial state for the computer's memory. When you run an Intcode program, make sure to start by initializing memory to the program's values. A position in memory is called an address (for example, the first value in memory is at "address 0").
+
+Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values used immediately after an opcode, if any, are called the instruction's parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the parameters. The instruction 99 contains only an opcode and has no parameters.
+
+The address of the current instruction is called the instruction pointer; it starts at 0. After an instruction finishes, the instruction pointer increases by the number of values in the instruction; until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters) for the add and multiply instructions. (The halt instruction would increase the instruction pointer by 1, but it halts the program instead.)
+
+"With terminology out of the way, we're ready to proceed. To complete the gravity assist, you need to determine what pair of inputs produces the output 19690720."
+
+The inputs should still be provided to the program by replacing the values at addresses 1 and 2, just like before. In this program, the value placed in address 1 is called the noun, and the value placed in address 2 is called the verb. Each of the two input values will be between 0 and 99, inclusive.
+
+Once the program has halted, its output is available at address 0, also just like before. Each time you try a pair of inputs, make sure you first reset the computer's memory to the values in the program (your puzzle input) - in other words, don't reuse memory from a previous attempt.
+
+Find the input noun and verb that cause the program to produce the output 19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.) */
+
+const findInputs = input => {
+  for (let i = 0; i <= 99; i++) {
+    for (let j = 0; j <= 99; j++) {
+      const noun = i;
+      const verb = j;
+      const data = [...input];
+      data[1] = noun;
+      data[2] = verb;
+
+      if (runProgram(data)[0] === 19690720) {
+        return 100 * noun + verb;
+      }
+    }
+  }
+};
+
+console.log(findInputs(inputs));
+
 module.exports = runProgram;
