e99fd3bb9baa78430bc3cf228cc9ff5d6dbe288e
susiekims <susie.kim@hotmail.com>
2019-12-07 10:26:56 -0500

    day6

--- day6/day6.test.js
+++ day6/day6.test.js
@@ -1,4 +1,4 @@
-const countOrbits = require("./index");
+const { countOrbits, getShortestTransfers } = require("./index");
 
 const input = [
   "COM)B",
@@ -17,5 +17,27 @@ const input = [
 describe("counting orbits", () => {
   test("it should return 42", () => {
     expect(countOrbits(input)).toBe(42);
+  });
+});
+
+const input2 = [
+  "COM)B",
+  "B)C",
+  "C)D",
+  "D)E",
+  "E)F",
+  "B)G",
+  "G)H",
+  "D)I",
+  "E)J",
+  "J)K",
+  "K)L",
+  "K)YOU",
+  "I)SAN"
+];
+
+describe("shortest transfers", () => {
+  test("it should return 4", () => {
+    expect(getShortestTransfers(input2)).toBe(4);
   });
 });
