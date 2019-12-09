508a9a55000780ad02fb51274ff9e256c8eaa298
susiekims <susie.kim@hotmail.com>
2019-12-08 20:20:15 -0500

    day8 part 2

--- day8/index.js
+++ day8/index.js
@@ -23,14 +23,18 @@ To make sure the image wasn't corrupted during transmission, the Elves would lik
 */
 
 const getInputs = require("../helpers/getInputs");
-const inputs = getInputs("./input.txt", "").map(Number);
+let inputs = getInputs("./input.txt", "").map(Number);
 
-const spaceImageEncoder = (image, width, height) => {
+const getLayers = (image, width, height) => {
   const layers = [];
-
   while (image.length > 0) {
     layers.push(image.splice(0, width * height));
   }
+  return layers;
+};
+
+const spaceImageEncoder = (image, width, height) => {
+  const layers = getLayers(image, width, height);
 
   const counts = layers.map(layer => {
     return layer.reduce((acc, curr) => {
@@ -47,4 +51,65 @@ const spaceImageEncoder = (image, width, height) => {
   return lowestZeros[1] * lowestZeros[2];
 };
 
-console.log(spaceImageEncoder(inputs, 25, 6));
+//console.log(spaceImageEncoder(inputs, 25, 6));
+/*
+--- Part Two ---
+Now you're ready to decode the image. The image is rendered by stacking the layers and aligning the pixels with the same positions in each layer. The digits indicate the color of the corresponding pixel: 0 is black, 1 is white, and 2 is transparent.
+
+The layers are rendered with the first layer in front and the last layer in back. So, if a given position has a transparent pixel in the first and second layers, a black pixel in the third layer, and a white pixel in the fourth layer, the final image would have a black pixel at that position.
+
+For example, given an image 2 pixels wide and 2 pixels tall, the image data 0222112222120000 corresponds to the following image layers:
+
+Layer 1: 02
+         22
+
+Layer 2: 11
+         22
+
+Layer 3: 22
+         12
+
+Layer 4: 00
+         00
+Then, the full image can be found by determining the top visible pixel in each position:
+
+The top-left pixel is black because the top layer is 0.
+The top-right pixel is white because the top layer is 2 (transparent), but the second layer is 1.
+The bottom-left pixel is white because the top two layers are 2, but the third layer is 1.
+The bottom-right pixel is black because the only visible pixel in that position is 0 (from layer 4).
+So, the final image looks like this:
+
+01
+10
+What message is produced after decoding your image?
+*/
+const colors = {
+  black: 0,
+  white: 1,
+  transparent: 2
+};
+
+const spaceImageDecoder = (image, width, height) => {
+  const layers = getLayers(image, width, height);
+
+  const pixels = layers.map((layer, index) => {
+    return layer.map(pixel => pixel[index]);
+  });
+
+  console.log(pixels[0].length);
+  const finalImage = pixels
+    .map(pixel => {
+      for (let i = 0; i < pixel.length; i++) {
+        if (pixel[i] !== 2) {
+          return pixel[i];
+        }
+      }
+    })
+    .join("");
+
+  console.log(finalImage.length);
+  return finalImage;
+};
+
+//inputs = [0, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 2, 0, 0, 0, 0];
+console.log(spaceImageDecoder(inputs, 25, 6));
